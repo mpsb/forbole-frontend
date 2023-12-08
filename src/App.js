954ae3c16ede4d41be29aa87b24a6ec17ccd0c9f
Matthew <mps.bilo@gmail.com>
2023-12-08 11:00:26 +0800

    feat: implemented check winner logic

--- src/App.js
+++ src/App.js
@@ -5,13 +5,42 @@ import * as styles from './index.module.css';
 const CELL_IDS = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9"];
 
 export function App() {
-  const isGameEndedState = localStorage.getItem("isGameEnded");
+  const isGameInProgressState = localStorage.getItem("isGameInProgress");
   const lastMoveState = localStorage.getItem("lastMoveState");
+  const movesPlayedState = localStorage.getItem("movesPlayed");
+  const winnerFromLastMove = localStorage.getItem("winnerFromLastMove");
   const savedGameState = localStorage.getItem("savedGameState");
-  const savedOGameState = localStorage.getItem("savedOGameState");
-  const savedXGameState = localStorage.getItem("savedXGameState");
-  const [ isGameEnded, setIsGameEnded ] = useState(isGameEndedState ? true : false);
+  const [ isGameInProgress, setIsGameInProgress ] = useState(isGameInProgressState ? true : false);
+  const [ movesPlayed, setMovesPlayed ] = useState(movesPlayedState ? parseInt(movesPlayedState) : 0);
   const [ moveState, setMoveState ] = useState(lastMoveState === "X" ? "O" : "X");
+  const [ winner, setWinner ] = useState("");
+
+  const checkWin = (gameState) => {
+    const winningCombinations = [
+      [0, 1, 2], // Top row
+      [3, 4, 5], // Middle row
+      [6, 7, 8], // Bottom row
+      [0, 3, 6], // Left column
+      [1, 4, 7], // Middle column
+      [2, 5, 8], // Right column
+      [0, 4, 8], // Diagonal from top-left to bottom-right
+      [2, 4, 6]  // Diagonal from top-right to bottom-left
+    ];
+
+    const cells = gameState;
+    let winner;
+
+    for (const combination of winningCombinations) {
+      const [a, b, c] = combination;
+
+      if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
+        winner = cells[a];
+        break;
+      }
+    }
+
+    return winner;
+  };
 
   const clearCell = (cell) => {
     const selectedCell = document.getElementById(cell);
@@ -20,30 +49,43 @@ export function App() {
     selectedCell.classList.remove(`${styles.unclickable}`);
     selectedCell.classList.remove(`${styles.red}`);
     selectedCell.classList.remove(`${styles.blue}`);
-  }
-
-  const initializeCell = (coordinate) => {
-    const move = coordinate[0];
-    const cell = coordinate.slice(1);
-    const selectedCellElement = document.getElementById(cell);
-
-    selectedCellElement.innerHTML = move;
-    selectedCellElement.classList.add(`${styles.unclickable}`);
-    selectedCellElement.classList.add(`${move === "X" ? styles.red : styles.blue}`);
   };
 
+  const initializeCell = (coordinate, index) => {
+    const selectedCellElement = document.getElementById("c" + (index+1));
+
+    if (coordinate === "") {
+      return;
+    }
+
+    selectedCellElement.innerHTML = coordinate;
+    selectedCellElement.classList.add(`${styles.unclickable}`);
+    selectedCellElement.classList.add(`${coordinate === "X" ? styles.red : styles.blue}`);
+  };
+
+  const makeAllCellsUnclickable = () => {
+    CELL_IDS.forEach((coordinate) => {
+      document.getElementById(coordinate).classList.add(`${styles.unclickable}`);
+    })
+  }; 
+
   const onClickCell = (e) => {
-    console.log("event", e.target);
-    if (e.target.innerHTML !== "") {
+    if (e.target.innerHTML !== "" && isGameInProgress || !isGameInProgress) {
       e.stopPropagation();
       e.preventDefault();
 
       return;
     }
 
-    setIsGameEnded(false);
-    localStorage.setItem("isGameEnded", "");
-    
+    setIsGameInProgress(true);
+    localStorage.setItem("isGameInProgress", "1");
+    setMovesPlayed((movesPlayed) => {
+      const newMovesPlayed = movesPlayed + 1;
+          localStorage.setItem("movesPlayed", newMovesPlayed);
+
+      return newMovesPlayed;
+    });
+
     const clickedCell = document.getElementById(e.target.id); 
     if (moveState === "X") {
       setMoveState("O");
@@ -63,66 +105,58 @@ export function App() {
   };
 
   const processGameState = (latestMove) => {
-    console.log("latestMove", latestMove);
+    const latestMovePosition = parseInt(latestMove.slice(2));
+    const latestMovePlayer = latestMove[0];
 
+    let savedGameStateArray = JSON.parse(savedGameState);
     if (savedGameState) {
-      let savedGameStateArray = JSON.parse(savedGameState);
-
-      if (savedGameStateArray.length > 8) {
-        localStorage.setItem("savedGameState", JSON.stringify([latestMove]));
-      } else {
-        savedGameStateArray.push(latestMove);
-        localStorage.setItem("savedGameState", JSON.stringify(savedGameStateArray));
-      }
-    } else {
-      localStorage.setItem("savedGameState", JSON.stringify([latestMove]));
+      savedGameStateArray[latestMovePosition - 1] = latestMovePlayer;
+      localStorage.setItem("savedGameState", JSON.stringify(savedGameStateArray));
     }
 
-    if (latestMove[0] === "X") {
-      let savedXGameStateArray = JSON.parse(savedXGameState);
+    const winState = checkWin(savedGameStateArray); 
 
-
-      if (savedXGameStateArray.length > 4) {
-        localStorage.setItem("savedXGameState", JSON.stringify([latestMove]));
-      } else {
-        savedXGameStateArray.push(latestMove);
-        localStorage.setItem("savedXGameState", JSON.stringify(savedXGameStateArray));
-      }
-    } else {
-      let savedOGameStateArray = JSON.parse(savedOGameState);
-
-      if (savedOGameStateArray.length > 4) {
-        localStorage.setItem("savedOGameState", JSON.stringify([latestMove]));
-      } else {
-        savedOGameStateArray.push(latestMove);
-        localStorage.setItem("savedOGameState", JSON.stringify(savedOGameStateArray));
-      }
+    if (winState) {
+      setWinner(winState);
+      setIsGameInProgress(false);
+      makeAllCellsUnclickable();
+      localStorage.setItem("winnerFromLastMove", "1");
     }
   };
 
   const resetGame = () => {
     setMoveState("X");
-    localStorage.setItem("savedGameState", JSON.stringify([]));
-    localStorage.setItem("savedOGameState", JSON.stringify([]));
-    localStorage.setItem("savedXGameState", JSON.stringify([]));
+    localStorage.setItem("savedGameState", JSON.stringify(Array(9).fill("")));
     localStorage.setItem("lastMoveState", "");
-    localStorage.setItem("isGameEnded", "1");
+    localStorage.setItem("isGameInProgress", "1");
+    localStorage.setItem("winnerFromLastMove", "");
+    localStorage.setItem("movesPlayed", 0);
 
     CELL_IDS.forEach((cell) => clearCell(cell));
 
-    setIsGameEnded(true);
+    setIsGameInProgress(true);
+    setWinner("");
+    setMovesPlayed(0);
   }
 
   useEffect(() => {
     const savedGameStateArray = JSON.parse(savedGameState);
-
-    if (savedGameStateArray.length > 0) {
-      savedGameStateArray.forEach((coordinate) => initializeCell(coordinate));
+    
+    if (!winnerFromLastMove) {
+      savedGameStateArray.forEach((coordinate, index) => initializeCell(coordinate, index));
+    } else {
+      resetGame();
     }
   }, []);
 
+  useEffect(() => {
+    if (movesPlayed >= 9 && winner === "") {
+      setIsGameInProgress(false);
+    }
+  }, [movesPlayed, winner]);
+
   return <div className={styles.gameContainer}>
-    <h1 style={{ textAlign: 'center', marginBottom: 0 }}>The most satisfying<br/>Tic-Tac-Toe Game</h1>
+    <h1 style={{ textAlign: 'center', marginBottom: 0 }}>The most satisfying<br/>Tic-Tac-Toe Game.</h1>
     <p style={{ textAlign: 'center', margin: 0 }}>Click any of the cells to start a game.<br/>Or click below to start a game with the AI.</p>
     <div className={styles.ticTacToeContainer}>
       <div className={styles.ticTacToeGrid}>
@@ -143,6 +177,8 @@ export function App() {
         </div>
       </div>
     </div>
-    {isGameEnded ? null : <button className={styles.secondaryButton} onClick={resetGame}>Reset game</button>}
+    {movesPlayed >= 9 && winner === "" ? "No winner." : null}
+    {!isGameInProgress && winner !== "" ? <p style={{ textAlign: 'center' }}>{`${winner} has won.`}</p> : null}
+    <button className={styles.secondaryButton} onClick={resetGame}>{isGameInProgress ? "Reset game" : "Play again"}</button>
   </div>;
 }
